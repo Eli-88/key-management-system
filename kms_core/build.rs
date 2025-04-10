598302cad3529e6ca5a558b059134b93231fa439
eli <elilim88@gmail.com>
2025-04-10 20:31:58 +0800

    added checks for target os for build script

--- kms_core/build.rs
+++ kms_core/build.rs
@@ -1,11 +1,23 @@
 extern crate cc;
 
-fn main() {
-    println!("cargo:rustc-link-lib=framework=Security");
-    println!("cargo:rustc-link-lib=framework=CoreFoundation");
-    println!("cargo:rerun-if-changed=build.rs");
+use std::env;
 
-    cc::Build::new()
-        .file("src/kms_core.c")
-        .compile("kms_core")
+fn main() {
+    match env::var("CARGO_CFG_TARGET_OS") {
+        Ok(target_os) => match target_os.as_str() {
+            "macos" => {
+                println!("cargo:rustc-link-lib=framework=CoreFoundation");
+                println!("cargo:rerun-if-changed=build.rs");
+                println!("cargo:rustc-link-lib=framework=Security");
+
+                cc::Build::new()
+                    .file("src/kms_core.c")
+                    .compile("kms_core");
+            }
+            _ => panic!("Unsupported OS"),
+        }
+        _ => {
+            panic!("Target OS not found");
+        }
+    }
 }
